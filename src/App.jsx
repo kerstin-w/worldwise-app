dfae8ac540cdfc3fad939fcdf0c49ffb1047ae54
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-05 16:43:43 +0200

    feat: Create Login page and css styling for it

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/Applayout";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" elemtn={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
