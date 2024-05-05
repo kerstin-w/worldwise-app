1ed30adb2b317bef256b074c6ca35613aa52c42e
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-05 17:22:49 +0200

    feat: Create basic CityList component add create css for component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/Applayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
@@ -16,8 +17,8 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
