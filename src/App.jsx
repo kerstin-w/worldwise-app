a301bebe9487bd12163ecc85bf73904ac1e0ca94
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-05 18:02:25 +0200

    feat Render city list in CityList component. Utilized mapping over 'cities' array to dynamically render CityItem components within a styled 'ul' element in the CityList component.

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import CityList from "./components/CityList";
 const BASE_URL = "http://localhost:9000";
 
 function App() {
-  const [cities, setCities] = useState({});
+  const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,18 @@
+import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
 
 function CityList({ cities, isLoading }) {
-  return <ul className={styles.cityList}>LIST</ul>;
+  if (isLoading) {
+    return <Spinner />;
+  }
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
