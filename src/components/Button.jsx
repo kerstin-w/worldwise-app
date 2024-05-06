a4b0e5eb3edd9f6075302b5ca4e22e3866558c4b
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-06 20:10:19 +0200

    docs: Add Comment to Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,12 @@
 import styles from "./Button.module.css";
 
+/**
+ * The Button component renders a button element with specified children, onClick
+ * function, and type styling.
+ * @returns The `Button` component is being returned. It is a button element with the specified
+ * `children`, `onClick` event handler, and `type` class name. The class names are dynamically
+ * generated based on the `styles` object.
+ */
 function Button({ children, onClick, type }) {
   return (
     <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
