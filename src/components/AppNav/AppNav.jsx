833419e52df8a724c70c24d4e394afb6237bd066
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-08 22:08:17 +0200

    docs: Add comment to AppNav function

--- src/components/AppNav/AppNav.jsx
+++ src/components/AppNav/AppNav.jsx
@@ -2,6 +2,11 @@ import { NavLink } from "react-router-dom";
 
 import styles from "./AppNav.module.css";
 
+/**
+ * The AppNav renders a navigation bar with links to "Cities" and "Countries".
+ * @returns The `AppNav` component is being returned. It is a navigation component that renders a
+ * `<nav>` element with a list of links to "Cities" and "Countries".
+ */
 function AppNav() {
   return (
     <nav className={styles.nav}>
