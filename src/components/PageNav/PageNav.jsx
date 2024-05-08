3b97a68719af0c9ed82ddcddba9c741b20bea123
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-08 22:18:07 +0200

    docs: Add comments to PagNav

--- src/components/PageNav/PageNav.jsx
+++ src/components/PageNav/PageNav.jsx
@@ -1,8 +1,15 @@
 import { NavLink } from "react-router-dom";
 
-import Logo from "../Logo/Logo";
 import styles from "./PageNav.module.css";
+import Logo from "../Logo/Logo";
 
+/**
+ * The `PageNav` is a navigation component with links to Pricing, Product, and Login
+ * pages.
+ * @returns The `PageNav` function is returning a navigation component that includes a logo and a list
+ * of navigation links. The navigation links include "Pricing", "Product", and a "Login" link styled as
+ * a call-to-action button.
+ */
 function PageNav() {
   return (
     <nav className={styles.nav}>
