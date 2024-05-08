70b9cbf81ece1d8c3484625228d0f83405f24933
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-08 21:26:59 +0200

    feat: Implement the City card to display all city information.

--- src/components/Citiy/City.jsx
+++ src/components/Citiy/City.jsx
@@ -2,15 +2,19 @@ import { useEffect } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 import useCities from "../../hooks/useCities";
 
-//import styles from "./City.module.css";
-/*
+import styles from "./City.module.css";
+
+/**
+ * The `formatDate` function formats a given date into a string with the day, month, year, and weekday
+ * in English.
+ */
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
     month: "long",
     year: "numeric",
     weekday: "long",
-  }).format(new Date(date));*/
+  }).format(new Date(date));
 
 function City() {
   const { id } = useParams();
@@ -23,9 +27,34 @@ function City() {
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
-    <div>
-      <h1>City{id}</h1>
-      <p>Position:</p>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+      <div></div>
     </div>
   );
 }
