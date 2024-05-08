e22b85384f244ae50d8f37623eb1130af684d0fb
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-08 20:42:34 +0200

    refactor: Extract Cities functionality into separate context and custom hook. - Moved the functionality related to fetching cities data out of Apps.jsx into a new context named CitiesContext. - Created a custom hook named useCities to access cities data and loading state. - Improved code organization and separation of concerns.

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
+
+export const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Failed to fetch data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
