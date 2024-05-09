2f48eddb52e566250bd1d72465c78a0b38cfa38e
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-09 19:44:49 +0200

    fix: revert back to original import

--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -1,4 +1,4 @@
-import styles from "src/pages/AppLayout/AppLayout.module.css";
+import styles from "./Applayout.module.css";
 import SideBar from "../../components/SideBar/SideBar";
 import Map from "../../components/Map/Map";
 import User from "../../components/User/User";
