a42df7fbec6acd183e2efbcca412d4479d3cb53e
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-09 19:42:34 +0200

    fix: Change path to absolute for testing purposes

--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -1,11 +1,11 @@
-//import styles from "./Applayout.module.css";
+import styles from "src/pages/AppLayout/AppLayout.module.css";
 import SideBar from "../../components/SideBar/SideBar";
 import Map from "../../components/Map/Map";
 import User from "../../components/User/User";
 
 function AppLayout() {
   return (
-    <div>
+    <div className={styles.app}>
       <SideBar />
       <Map />
       <User />
