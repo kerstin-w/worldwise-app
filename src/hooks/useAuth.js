540315ed0080b8b490b9df5c80a2abe979871953
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-09 17:44:39 +0200

    feat: Implement custom hook and FakeAuthContext for user authentication
    - Created useAuth custom hook to provide access to authentication context within components.
    - Implemented AuthProvider component to manage user authentication state using a reducer.
    - Defined initial state for user authentication with null user and isAuthenticated set to false.
    - Implemented login and logout functions within AuthProvider to manage user authentication status based on provided email and password.
    - Utilized createContext and useReducer hooks to create the AuthContext and manage authentication state respectively.
    - Added FAKE_USER constant to simulate user authentication with a predefined user object for testing purposes.

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/FakeAuthContext";
+
+/**
+ * The useAuth hook is designed to ensure that it is used within an AuthProvider context in
+ * JavaScript.
+ */
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("useAuth must be used within an AuthProvider");
+}
+
+export default useAuth;
