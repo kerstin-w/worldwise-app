bc043780479d3568b97d7d325a45aab39e7aa167
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-05-09 18:08:08 +0200

    fix: Add return context to useAuth hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -9,6 +9,7 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("useAuth must be used within an AuthProvider");
+  return context;
 }
 
 export default useAuth;
